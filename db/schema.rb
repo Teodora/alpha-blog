7924c36961c7148ec5a22d7064846bf992695125
Teodora Moldovan <dora@indigenious.ro>
2016-10-11 13:08:01 +0300

    Adding admin functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010080343) do
+ActiveRecord::Schema.define(version: 20161011095753) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "description"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20161010080343) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
