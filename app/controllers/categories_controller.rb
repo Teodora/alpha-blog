32098d76495072cdd7a08de60d274014abbddd9c
Teodora Moldovan <dora@indigenious.ro>
2016-10-13 14:40:58 +0300

    Edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
 	before_action :require_admin, except: [ :index, :show ]
 
 	def index
-		@categories = Category.paginate(page: params[:page], per_page: 2)
+		@categories = Category.paginate(page: params[:page], per_page: 5)
 	end
 
 	def new
@@ -19,9 +19,23 @@ class CategoriesController < ApplicationController
 		end
 	end
 
+	def edit
+		@category = Category.find(params[:id])
+	end
+
+	def update
+		@category = Category.find(params[:id])
+		if @category.update(category_params)
+			flash[:success] = "Category name was successfully updated"
+			redirect_to category_path(@category)
+		else
+			render 'edit'
+		end
+	end
+
 	def show
 		@category = Category.find(params[:id])
-		@category_articles = @category.articles.paginate(page: params[:page], per_page: 1)
+		@category_articles = @category.articles.paginate(page: params[:page], per_page: 2)
 
 	end
 
